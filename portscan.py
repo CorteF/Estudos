cbc6325cff72490d1bef970cbb3c5d438aa55a3c
CorteF <129459194+CorteF@users.noreply.github.com>
2023-09-05 19:35:55 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,9 +1,25 @@
 import socket
+import concurrent.futures
 
 ip = input("Digite o IP:")
-for porta in range(1,65535):
-        meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if meusocket.connect_ex((ip,porta)) == 0:
-                banner = meusocket.recv(1024)
-                print("Porta",porta,"[ABERTA]",banner)
-                meusocket.close()
+
+
+def scan_port(port):
+    meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    meusocket.settimeout(1)
+
+    if meusocket.connect_ex((ip, port)) == 0:
+        try:
+            banner = meusocket.recv(1024)
+            print("Porta", port, "[ABERTA]", banner.decode('utf-8'))
+        except socket.timeout:
+            print("Porta", port, "[ABERTA] (Timeout)")
+        finally:
+            meusocket.close()
+
+
+# Use um ThreadPoolExecutor para paralelizar o escaneamento
+with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+    # Escaneie as portas em paralelo
+    executor.map(scan_port, range(1, 65536))
+
